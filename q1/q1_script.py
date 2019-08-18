82af399b0b6a440cb3425f45adb8c6ecc50a84ef
AadilMehdi Sanchawala <aadilmehdis@gmail.com>
2019-08-18 11:44:51 +0530

    Adding the code for the 1st question

---
+++ q1/q1_script.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+# Importing libraries
+import numpy as np
+import load_points
+import matplotlib.pyplot as plt
+
+
+
+
+# World to Camera transformation of Frame
+# 1. Translate by [27,6,-8]
+# 2. Rotate by -90˚ along the Z axis
+# 3. Rotate by -90˚ along the X axis
+
+# Thus,
+#     Pworld = [ 1 0 0 27 ] [ 0 1 0 0 ] [1  0 0 0] Pcamera
+#              [ 0 1 0  6 ] [-1 0 0 0 ] [0  0 1 0]
+#              [ 0 0 1 -8 ] [ 0 0 1 0 ] [0 -1 0 0]
+#              [ 0 0 0  1 ] [ 0 0 0 1 ] [0  0 0 1]
+
+
+
+
+# Debug Code V1
+# -------------
+# T1 = np.array([[1,0,0,-0.27],[0,1,0,-0.06],[0,0,1,0.08],[0,0,0,1]])
+# R1 = np.array([[0,1,0,0],[-1,0,0,0],[0,0,1,0],[0,0,0,1]])
+# R2 = np.array([[1,0,0,0],[0,0,1,0],[0,-1,0,0],[0,0,0,1]])
+# K = np.array([[7.215377e+02, 0.000000e+00, 6.095593e+02],[0.000000e+00, 7.215377e+02, 1.728540e+02],[0.000000e+00, 0.000000e+00, 1.000000e+00]])
+
+# Debug Code V2
+# -------------
+# T1 = np.array([[1,0,0,-0.27],[0,1,0,-0.06],[0,0,1,0.08],[0,0,0,1]])
+# R1 = np.array([[0,-1,0,0],[1,0,0,0],[0,0,1,0],[0,0,0,1]])
+# R2 = np.array([[0,0,-1,0],[0,1,0,0],[1,0,0,0],[0,0,0,1]])
+# K = np.array([[7.215377e+02, 0.000000e+00, 6.095593e+02],[0.000000e+00, 7.215377e+02, 1.728540e+02],[0.000000e+00, 0.000000e+00, 1.000000e+00]])
+
+# M = np.matmul(np.matmul(T1,R1),R2)
+# M = np.matmul(np.matmul(R1,R2),T1)
+# M = np.matmul(np.matmul(R1,R2),T1)
+# K = np.append(K, np.zeros((K.shape[0],1)), axis=1)
+# P = np.matmul(K,M)
+
+
+# Current Code 
+
+# Pushing the world origin by the Camera's position i.e. Making the Camera the origin of the world
+T  = np.array([[1,0,0,-0.27],[0,1,0,-0.06],[0,0,1,0.08]])
+
+# Rotate -90˚ along Z axis
+R1 = np.array([[0,-1,0],[1,0,0],[0,0,1]])
+
+# Rotate -90˚ along X axis
+R2 = np.array([[1,0,0],[0,0,-1],[0,1,0]])
+
+# Obtain the compound rotation
+R  = np.matmul(R1,R2)
+
+# Intrinsic Camera Calibration Matrix
+K  = np.array([[7.215377e+02, 0.000000e+00, 6.095593e+02],[0.000000e+00, 7.215377e+02, 1.728540e+02],[0.000000e+00, 0.000000e+00, 1.000000e+00]])
+
+# Getting the Projection Matrix
+P = np.matmul(K,np.matmul(R,T))
+
+
+# Getting the Lidar points
+points = load_points.load_velodyne_points('lidar-points.bin')
+# Augmenting 1 for every vector to Homogenise the Vectors
+points = np.append(points, np.ones((points.shape[0],1)), axis=1)
+
+# Projecting Lidar points onto the image plane
+image_points = np.matmul(P, points.T)
+image_points = image_points.T
+
+
+# Plotting Image Points
+image_points[:,0] /= image_points[:,2]
+image_points[:,1] /= image_points[:,2]
+image_points[:,2] /= image_points[:,2]
+plt.scatter(image_points[:,0], image_points[:,1],s=0.2)
+plt.ylim((0, 512))
+plt.xlim((0, 1382))
+plt.show()
+
+
+
+
+
+
+
+
+
