ab35ffa6dc698cd22288e41cddc588d93a34376b
RahulSajnani <rahulsajnani2010@hotmail.com>
2019-08-20 18:31:18 +0530

    q2 5 deg rotation done

--- q2/q2.py
+++ q2/q2.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv
-x = [[829], [298], [1]]
+x = [[835], [305], [1]]
 K = np.mat(([7.2153e+02,0,6.0955e+02],[0,7.2153e+02,1.7285e+02],[0,0,1]))
 B = np.dot(np.linalg.inv(K),x)
 
@@ -18,14 +18,19 @@ carHeight = 1.38
 carWidth = 1.51
 carLength = 4.10
 
+
+#5 degree rotation along y clockwise
+
+Rot = np.mat(([np.cos(5*np.pi/180),0,-np.sin(5*np.pi/180)],[0,1,0],[ np.sin(5*np.pi/180),0,np.cos(5*np.pi/180)]))
+ 
 # KITTI dataset y is downwards x is right and z is forward (into the image)
-WorldPoint2 = [[0],[0],[carLength]] + WorldPoint
-WorldPoint3 = [[carWidth],[0],[0]] + WorldPoint
-WorldPoint4 = [[0],[-carHeight],[0]] + WorldPoint
-WorldPoint5 = [[carWidth],[-carHeight],[0]] + WorldPoint
-WorldPoint6 = [[0],[-carHeight],[carLength]] + WorldPoint
-WorldPoint7 = [[carWidth],[0],[carLength]] + WorldPoint
-WorldPoint8 = [[carWidth],[-carHeight],[carLength]] + WorldPoint
+WorldPoint2 = np.matmul(Rot,[[0],[0],[carLength]]) + WorldPoint
+WorldPoint3 = np.matmul(Rot,[[carWidth],[0],[0]]) + WorldPoint
+WorldPoint4 = np.matmul(Rot,[[0],[-carHeight],[0]]) + WorldPoint
+WorldPoint5 = np.matmul(Rot,[[carWidth],[-carHeight],[0]]) + WorldPoint
+WorldPoint6 = np.matmul(Rot,[[0],[-carHeight],[carLength]]) + WorldPoint
+WorldPoint7 = np.matmul(Rot,[[carWidth],[0],[carLength]]) + WorldPoint
+WorldPoint8 = np.matmul(Rot,[[carWidth],[-carHeight],[carLength]]) + WorldPoint
 
 WorldPoints = np.array((WorldPoint,WorldPoint2,WorldPoint3,WorldPoint4,WorldPoint5,WorldPoint6,WorldPoint7,WorldPoint8))
 
@@ -38,7 +43,7 @@ print(WorldPoints[0])
 pic = plt.imread("image.png")
 plt.imshow(pic)
  
-edges = np.array([1, 3, 5, 4, 1, 2, 6, 8, 7, 3, 5, 8, 6, 4]) 
+edges = np.array([1, 3, 5, 4, 1, 2, 6, 8, 7, 3, 5, 8, 6, 4, 6, 2, 7]) 
 for i in range(8):
     d2point = np.matmul(K,WorldPoints[i])
     d2point = d2point/d2point[2]
@@ -53,6 +58,6 @@ for i in range(edges.size):
 
 print(linemat[0])    
 
-plt.plot(np.transpose(linemat[0]),np.transpose(linemat[1]))    
+plt.plot(np.transpose(linemat[0]),np.transpose(linemat[1]),linewidth="3")    
 
 plt.show()
